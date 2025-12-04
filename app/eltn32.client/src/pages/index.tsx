fdd8eaf76a9cc15f4b2d9a04efefce49b80a42b7
Hector Miranda <marcetux@gmail.com>
2025-12-04 11:39:23 -0800

    Removing unused project and fixing vercel configs

--- app/eltn32.client/src/pages/index.tsx
+++ app/eltn32.client/src/pages/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Home() {
   return (
     <div className="prose">
-      <h1>Welcome to Logic Tools</h1>
+      <h1>Welcome to Logic Tools for ELTN132</h1>
       <p>Select a topic from the left to get started. This is a placeholder home page.</p>
       <p>To build the project, run <code>npm run build</code>.</p>
     </div>
